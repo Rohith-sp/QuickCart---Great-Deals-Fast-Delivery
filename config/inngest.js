992c28e15b509ee42ab83ba09dd28b7f4588236a
Rohith Panchamukhi <rohithpanchamukhi@gmail.com>
2025-11-21 18:36:44 +0530

    Fixed inngest.js

--- config/inngest.js
+++ config/inngest.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import { connect } from "mongoose";
+import connectDB from "./db.js";
+import User from "../models/User.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickkart-next" });
@@ -30,46 +32,46 @@ export const syncUserCreation = inngest.createFunction(
     await connectDB();
     await User.create(userData);
 
-    
+
   }
 );
 //Inngest function to update user data in database
 export const syncUserUpdation = inngest.createFunction(
-    {
-        id: "sync-update-user-from-clerk",
-    },
-    {
-        event: "clerk/user.updated",
-    },
-    async ({ event }) => {
-        const {
-            id,
-            first_name,
-            last_name,
-            email_addresses,
-            image_url,
-        } = event.data;
-        const userData = {
-            name: `${first_name || ""} ${last_name || ""}`.trim(),
-            email: email_addresses?.[0]?.email_address,
-            imageUrl: image_url,
-        };
-        await connectDB();
-        await User.findByIdAndUpdate(id, userData);
-    }
+  {
+    id: "sync-update-user-from-clerk",
+  },
+  {
+    event: "clerk/user.updated",
+  },
+  async ({ event }) => {
+    const {
+      id,
+      first_name,
+      last_name,
+      email_addresses,
+      image_url,
+    } = event.data;
+    const userData = {
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
+      email: email_addresses?.[0]?.email_address,
+      imageUrl: image_url,
+    };
+    await connectDB();
+    await User.findByIdAndUpdate(id, userData);
+  }
 );
 
 //Inngest function to delete user from the database
 export const syncUserDeletion = inngest.createFunction(
-    {
-        id: "sync-delete-user-from-clerk",
-    },
-    {
-        event: "clerk/user.deleted",
-    },
-    async ({ event }) => {
-        const { id } = event.data;
-        await connectDB();
-        await User.findByIdAndDelete(id);
-    }
+  {
+    id: "sync-delete-user-from-clerk",
+  },
+  {
+    event: "clerk/user.deleted",
+  },
+  async ({ event }) => {
+    const { id } = event.data;
+    await connectDB();
+    await User.findByIdAndDelete(id);
+  }
 );
